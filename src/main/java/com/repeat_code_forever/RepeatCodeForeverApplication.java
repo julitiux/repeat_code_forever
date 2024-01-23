5ec1aec5fcbafa54d4cd12948ad7d2d2d134dc9c
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-22 23:25:19 -0600

    starting a new project: repeat_code_forever

---
+++ src/main/java/com/repeat_code_forever/RepeatCodeForeverApplication.java
@@ -0,0 +1,13 @@
+package com.repeat_code_forever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepeatCodeForeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepeatCodeForeverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repeat_code_forever/RepeatCodeForeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repeat_code_forever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepeatCodeForeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
