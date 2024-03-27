66d66fad200ade343b70b9a2fdd33c1242cceb94
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-03-26 23:37:18 -0600

    implement class StringConcatenated with a method static and a pair method publics; firstString and secondString

--- src/main/java/com/repeat_code_forever/StringConcatenated.java
+++ src/main/java/com/repeat_code_forever/StringConcatenated.java
@@ -1,4 +1,29 @@
 package com.repeat_code_forever;
 
+import java.util.function.Consumer;
+
 public class StringConcatenated {
+
+  static String finalString;
+
+  private StringConcatenated() {
+    finalString = "StartingWithConstructor";
+  }
+
+  public StringConcatenated firstString(String firstString) {
+    finalString += firstString;
+    return this;
+  }
+
+  public StringConcatenated secondString(String secondString) {
+    finalString += secondString;
+    return this;
+  }
+
+  public static String make(final Consumer<StringConcatenated> block) {
+    final StringConcatenated stringConcatenated = new StringConcatenated();
+    block.accept(stringConcatenated);
+    return finalString;
+  }
+
 }
