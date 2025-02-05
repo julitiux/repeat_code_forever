e76c3f24dad4e666773cbd3fadd45e0d5e9d40d0
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-02-04 23:04:51 -0600

    repear code using reduce with stream

--- src/test/java/com/repeat_code_forever/RepeatCodeTest.java
+++ src/test/java/com/repeat_code_forever/RepeatCodeTest.java
@@ -1,4 +1,19 @@
 package com.repeat_code_forever;
 
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class RepeatCodeTest {
+
+  @Test
+  void repeatCode() {
+    final List<BigDecimal> bigDecimals = List.of(new BigDecimal("1.0"), new BigDecimal("1.0"), new BigDecimal("1.0"));
+
+    final BigDecimal test = bigDecimals.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+
+    System.out.println(test);
+  }
+
 }
