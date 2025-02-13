0980fe8596089aa2a210191fe3da72752bbca967
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-02-12 22:39:10 -0600

    implement method firstMethod and return a true

--- src/test/java/com/repeat_code_forever/LazyEvaluatorTest.java
+++ src/test/java/com/repeat_code_forever/LazyEvaluatorTest.java
@@ -2,10 +2,18 @@ package com.repeat_code_forever;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Supplier;
+
 public class LazyEvaluatorTest {
 
   @Test
   void testLazy(){
-    
+
+    Supplier<Boolean> first = this::firstMethod;
+
+  }
+
+  private Boolean firstMethod(){
+    return true;
   }
 }
