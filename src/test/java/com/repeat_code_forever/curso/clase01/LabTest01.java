c1e9cd9d8474fe3243c4b94cf0cfe92858e39c21
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-03-06 23:00:34 -0600

    add BiFunction test

---
+++ src/test/java/com/repeat_code_forever/curso/clase01/LabTest01.java
@@ -0,0 +1,14 @@
+package com.repeat_code_forever.curso.clase01;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.BiFunction;
+
+public class LabTest01 {
+
+  @Test
+  void lab01() {
+    BiFunction<Integer, Integer, Integer> suma = (a, b) -> a + b;
+    System.out.println(suma.apply(10, 2));
+  }
+}
