00afe5c6cb32316a38799c79136dfaa2cf8490db
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-03-06 23:11:07 -0600

    add Consumer with a method reference

--- src/test/java/com/repeat_code_forever/curso/clase01/LabTest01.java
+++ src/test/java/com/repeat_code_forever/curso/clase01/LabTest01.java
@@ -12,7 +12,7 @@ public class LabTest01 {
     BiFunction<Integer, Integer, Integer> suma = Integer::sum;
     System.out.println(suma.apply(10, 2));
 
-    Consumer<String> imprimir = it -> System.out.println(it);
+    Consumer<String> imprimir = System.out::println;
     imprimir.accept("Hola Inmundo");
   }
 }
