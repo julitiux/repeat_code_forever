e6e9f581a3358c41ca8b3b3aa913310863fd705c
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-03-06 23:09:40 -0600

    add Consumer with System.out.println

--- src/test/java/com/repeat_code_forever/curso/clase01/LabTest01.java
+++ src/test/java/com/repeat_code_forever/curso/clase01/LabTest01.java
@@ -3,6 +3,7 @@ package com.repeat_code_forever.curso.clase01;
 import org.junit.jupiter.api.Test;
 
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 
 public class LabTest01 {
 
@@ -10,5 +11,8 @@ public class LabTest01 {
   void lab01() {
     BiFunction<Integer, Integer, Integer> suma = Integer::sum;
     System.out.println(suma.apply(10, 2));
+
+    Consumer<String> imprimir = it -> System.out.println(it);
+    imprimir.accept("Hola Inmundo");
   }
 }
