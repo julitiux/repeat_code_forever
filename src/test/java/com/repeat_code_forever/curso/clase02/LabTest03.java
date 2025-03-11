39e4f56886a65e699d436722e077033e75f00efb
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-03-11 00:12:28 -0600

    implement a rest with a interface anonymous

--- src/test/java/com/repeat_code_forever/curso/clase02/LabTest03.java
+++ src/test/java/com/repeat_code_forever/curso/clase02/LabTest03.java
@@ -8,15 +8,21 @@ public class LabTest03 {
 
   public static void main(String[] args) {
 
-    CalculaOperacionBasica suma = new CalculaOperacionBasica(){
+    CalculaOperacionBasica suma = new CalculaOperacionBasica() {
       @Override
       public int operacion(int param1, int param2) {
         return param1 + param2;
       }
     };
-
     System.out.println(suma.operacion(5, 4));
 
-  }
+    CalculaOperacionBasica resta = new CalculaOperacionBasica() {
+      @Override
+      public int operacion(int param1, int param2) {
+        return param1 - param2;
+      }
+    };
+    System.out.println(resta.operacion(10, 2));
 
+  }
 }
