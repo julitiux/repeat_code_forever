558ea75cf5a220476b065491151e05460e57d02f
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-03-11 00:01:44 -0600

    add test using FunctionalInterface and method anonimous

--- src/test/java/com/repeat_code_forever/curso/clase01/LabTest02.java
+++ src/test/java/com/repeat_code_forever/curso/clase01/LabTest02.java
@@ -1,9 +1,14 @@
 package com.repeat_code_forever.curso.clase01;
 
+@FunctionalInterface
+interface Operacion {
+  int ejecutar(int a, int b);
+}
+
 public class LabTest02 {
 
-  @FunctionalInterface
-  interface Operacion {
-    int ejecutar(int a, int b);
+  public static void main(String[] args) {
+    Operacion multiplicacion = (x, y) -> x * y;
+    System.out.println(multiplicacion.ejecutar(3, 4));
   }
 }
