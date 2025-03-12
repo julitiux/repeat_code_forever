5ec01c115acfafe821248818744bcbce4c03ebc9
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-03-11 23:57:42 -0600

    add clase03:TestLab01 Function superior order

---
+++ src/test/java/com/repeat_code_forever/curso/clase03/TestLab01.java
@@ -0,0 +1,17 @@
+package com.repeat_code_forever.curso.clase03;
+
+import java.util.function.Function;
+
+public class TestLab01 {
+
+  public static int operator(int number, Function<Integer, Integer> operation) {
+    return operation.apply(number);
+  }
+
+  public static void main(String[] args) {
+
+    Function<Integer, Integer> duplicate = x -> x * 2;
+
+    System.out.println(operator(10, duplicate));
+  }
+}
